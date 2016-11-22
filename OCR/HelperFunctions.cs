f4ed47775e8393327b11f64c1e8283b19544cedb
Nada Alaa <nadaalaa.na@gmail.com>
2016-11-22 21:54:00 +0200

    Added OCR class

---
+++ OCR/HelperFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCR
+{
+    class HelperFunctions
+    {
+        public static double[] ToDoubleArray(string[] lines)
+        {
+            double[] ret = new double[lines.Length*lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (lines[i][j] == '1') ret[i * lines.Length + j] = 1;
+                    else ret[i * lines.Length + j] = 0;
+                }
+            }
+            return ret;
+        }
+        public static void Normalize(ref double[] input)
+        {
+            double mean = 0, absMax = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                mean += input[i];
+                absMax = Math.Max(absMax, input[i]);
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = (input[i] - mean) / absMax;
+            }
+        }
+    }
+}
---
+++ OCR/OCR.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCR
+{
+    class OCR
+    {
+        MultiLayerPerceptron MLP;
+        int num_Classes;
+        int imageSz;
+        double threshold;
+        string[] classes = {"G", "L", "R", "Y", "Z"};
+
+        public OCR(int[] layers, int num_Classes, double alpha, double threshold, TransferFunction.Type type)
+        {
+            imageSz = 30*30;
+            this.threshold = threshold;
+            this.num_Classes = num_Classes;
+            MLP = new MultiLayerPerceptron(layers, alpha, type);
+        }
+
+        public double Learn(int num_TrainingPerClass)
+        {
+            double meanSquareError = 0;
+            while (meanSquareError < threshold)
+            {
+                for (int i = 0; i < num_Classes; i++)
+                {
+                    for (int j = 1; j <= num_TrainingPerClass; j++)
+                    {
+                        String pattern = "img/" + classes[i] + "/" + j + ".txt";
+                        string[] lines = System.IO.File.ReadAllLines(pattern);
+                        double[] input = HelperFunctions.ToDoubleArray(lines);
+                        HelperFunctions.Normalize(ref input);
+
+                        double[] output = new double[num_Classes];
+                        for (int k = 0; k < num_Classes; k++)
+                        {
+                            if (k != i) output[k] = 0;
+                            else output[k] = 1;
+                        }
+                        meanSquareError += MLP.BackPropagate(input, output);
+                    }
+                }
+                meanSquareError /= num_Classes;
+            }
+            return meanSquareError;
+        }
+        public int recognize(String imgPath)
+        {
+            string[] lines = System.IO.File.ReadAllLines(imgPath);
+            double[] input = HelperFunctions.ToDoubleArray(lines);
+            double[] output = MLP.ForwardPropagate(input);
+            int classified = 0;
+            for (int i = 0; i < num_Classes; i++)
+            {
+                if (output[i] > output[classified]) classified = i;
+            }
+            return classified;
+        }
+    }
+}
