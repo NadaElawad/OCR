b0f4ecfbeeba2144acf1f0203d886538b06d1cc0
Nada Alaa <nadaalaa.na@gmail.com>
2016-11-22 17:11:00 +0200

    first commit

--- OCR/TransferFunction.cs
+++ OCR/TransferFunction.cs
@@ -8,7 +8,24 @@ namespace OCR
 {
     class TransferFunction
     {
-        public double evalute(double value);
-        public double evaluteDerivate(double value);
+        public static double evaluteSigmoid(double value) 
+	    {
+		    return 1 / (1 + Math.Pow(Math.E, - value));
+	    }
+
+	    public static double evaluteDerivateSigmoid(double value) 
+	    {
+		    return (value - Math.Pow(value, 2));
+	    }
+
+        public static double evaluteHyperbolic(double value) 
+	    {
+		    return Math.Tanh(value);
+	    }
+
+        public static double evaluteDerivateHyperbolic(double value) 
+	    {
+		    return 1 - Math.Pow(value, 2);
+	    }
     }
 }
