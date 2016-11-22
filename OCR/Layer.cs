b0d8e064c04d76756e7fa532ccb6ee9f2eb15ad0
Nada Alaa <nadaalaa.na@gmail.com>
2016-11-22 03:00:37 +0200

    adding files

---
+++ OCR/Layer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCR
+{
+    class Layer
+    {
+        public Neuron[] Neurons;
+	    public int Length;
+	
+	    /**
+	     * Layer of Neurons
+	     * 
+	     * @param l Layer Size
+	     * @param prev Size of the previous layer
+	     */
+	    public Layer(int length, int prev)
+	    {
+		    this.Length = length;
+		    Neurons = new Neuron[length];
+		
+		    for(int j = 0; j < Length; j++)
+			    Neurons[j] = new Neuron(prev);
+	    }
+    }
+}
---
+++ OCR/Neuron.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCR
+{
+    class Neuron
+    {
+        public double Value;
+        public double[] Weights;
+        public double Bias;
+        public double Delta;
+
+        public Neuron(int prevLayerSize)
+        {
+            Random rand = new Random();
+            Weights = new double[prevLayerSize];
+            Bias = rand.Next() / 10000000000000.0;
+            Delta = rand.Next() / 10000000000000.0;
+            Value = rand.Next() / 10000000000000.0;
+
+            for (int i = 0; i < Weights.Length; i++)
+                Weights[i] = rand.Next() / 10000000000000.0;
+        }
+    }
+}
